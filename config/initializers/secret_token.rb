eae27a5a74469ff9a7d9eb03a865911263914b0f
Jason Carter <jccarte@gmail.com>
2013-03-27 17:26:44 -0400

    Initial commit of conference scheduler app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenceScheduler::Application.config.secret_token = 'e990ba7bb96ad32d193ee02b12f3e24fb3f7bbe649dcf15adc2012a1eb1fec0588c9a128522f5936e4ff7c567e373fb6fa79e057f7845febe40c4708647053e1'
